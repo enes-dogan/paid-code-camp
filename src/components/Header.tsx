17679721cc5b1c8b75d9bb682c8c176d7d9e6f5f
enes-dogan <enesdogan210@gmail.com>
2023-08-07 15:53:24 +0300

    changing sites brand

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,16 +5,16 @@ const Header = () => {
     <header className={styles.header}>
       <div className={styles.logoBox}>
         <img
-          src='./src/img/logo-white.png'
+          src='./src/img/logo-whitee.png'
           alt='Logo'
           className={styles.logo}
         />
       </div>
       <div className={styles.headerTextBox}>
         <h1 className={styles.headingPrimary}>
-          <span className={styles.headingPrimaryMain}>Outdoors</span>
+          <span className={styles.headingPrimaryMain}>paidCodeCamp($)</span>
           <span className={styles.headingPrimarySub}>
-            is where life happens
+            a paid coding bootcamp (not freeCodeCamp)
           </span>
         </h1>
       </div>
